934300a926f302eb3a38a2f661d71ec962ed45c2
Carter Prince <carteraprince@gmail.com>
2017-11-28 23:26:52 -0500

    Added Grudger
    adds the grudger strat for the computer

--- prisoners-dilemma.py
+++ prisoners-dilemma.py
@@ -2,28 +2,42 @@ firstRound = True
 
 global playerScore
 global computerScore
+
+global computerIsContent
+
 playerScore = 0
 computerScore = 0
 
-round = 0
+round = 1
+
+computerIsContent = True
 
 global lastPlayerCooperate
 lastPlayerCooperate = None
 
-def calculateComputerChoice(lastPlayerCooperate):
+def tft():
 	if firstRound:
 		return True
 	else:
 		return lastPlayerCooperate
 
-while True:
+def grudger():
+	if computerIsContent:
+		return True
+	else:
+		return False
+
+
+
+while round <= 10:
 	playerChoice = input("Cooperate? (Y/N): ")
 	if playerChoice == "y" or playerChoice == "Y":
 		playerCooperate = True
 	if playerChoice == "n" or playerChoice == "N":
 		playerCooperate = False
 
-	computerCooperate = calculateComputerChoice(lastPlayerCooperate)
+	#change the function used to change behavior of the computer
+	computerCooperate = tft()
 
 	#calculate scores
 	if playerCooperate and computerCooperate:
@@ -35,15 +49,24 @@ while True:
 	if not playerCooperate and computerCooperate:
 		playerScore += 5
 		computerScore += 0 #just for show
+		computerIsContent = False
 	if not playerCooperate and not computerCooperate:
 		playerScore += 2
 		computerScore += 2
-
-	print("You Cooperated:" + str(playerCooperate))
-	print("Computer Cooperated:" + str(computerCooperate))
+		computerIsContent = False
+	print("")
+	print("You Cooperated: " + str(playerCooperate))
+	print("Computer Cooperated: " + str(computerCooperate) + "\n")
 
 	round += 1
-	print(playerScore)
-	print(computerScore)
+	print("Player Score: " + str(playerScore))
+	print("Computer Score: " + str(computerScore) + "\n")
 	firstRound = False
 	lastPlayerCooperate = playerCooperate
+
+if playerScore > computerScore:
+	print("You won by " + str(playerScore - computerScore) + " points.")
+elif playerScore == computerScore:
+	print("You tied with the computer.")
+else:
+	print("The computer beat you by " + str(computerScore - playerScore) + " points.")
