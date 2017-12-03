eca17a3ce58640698097263fa5bbe424e43d59c7
Carter Prince <carteraprince@gmail.com>
2017-12-03 13:57:11 -0500

    make systems more modular
    allows the use of different strategies against others

--- prisoners-dilemma.py
+++ prisoners-dilemma.py
@@ -1,72 +1,139 @@
+import random
+
 firstRound = True
 
-global playerScore
-global computerScore
+global p1Score
+global p2Score
 
-global computerIsContent
+p1Score = 0
+p2Score = 0
 
-playerScore = 0
-computerScore = 0
+global grudgerIsContent
 
 round = 1
 
-computerIsContent = True
+grudgerIsContent = True
 
-global lastPlayerCooperate
-lastPlayerCooperate = None
+global lastp1Cooperate
+lastp1Cooperate = None
+
+global lastp2Cooperate
+lastp2Cooperate = None
+
+#each function outputs cooperate as True and not cooperate as False
+def allcooperate():
+	return True
+
+def allnotcooperate():
+	return False
+
+def rand():
+	return bool(random.getrandbits(1))
+
+def player():
+	while True:
+		playerChoice = input("Cooperate? (Y/N): ")
+		if playerChoice.lower() == "y" or playerChoice.lower() == "yes":
+			return True
+		if playerChoice.lower() == "n" or playerChoice.lower() == "no":
+			return False
+		else:
+			print("Not a valid answer.\n")
+			continue
+
+def grudger():
+	if grudgerIsContent:
+		return True
+	else:
+		return False
 
 def tft():
 	if firstRound:
 		return True
 	else:
-		return lastPlayerCooperate
+		if playermap["p2"] == "tft":
+			return lastp1Cooperate
+		else:
+			return lastp2Cooperate
 
-def grudger():
-	if computerIsContent:
-		return True
-	else:
+
+def oppositetft():
+	if firstRound:
 		return False
+	else:
+		if playermap["p2"] == "tft":
+			return not lastp1Cooperate
+		else:
+			return not lastp2Cooperate
 
 
+# Full list of strategies (input them like this)
+# "p1": tft,
+# "p2": player
+# replace tft and player with the strategies you want to use
+
+# grudger
+# rand
+# player
+# tft
+# allcooperate
+# allnotcooperate
+# oppositetft
+
+strats = [grudger, rand, player, tft, allcooperate, allnotcooperate, oppositetft]
+
+playermap = {
+
+	"p1": rand,
+	"p2": rand
+
+}
 
 while round <= 10:
-	playerChoice = input("Cooperate? (Y/N): ")
-	if playerChoice.lower() == "y":
-		playerCooperate = True
-	if playerChoice.lower() == "n":
-		playerCooperate = False
 
-	#change the function used to change behavior of the computer
-	computerCooperate = tft()
+	#change the function used to change behavior of player1
+
+
+	p1Cooperate = playermap["p1"]()
+
+	
+	#change the function used to change behavior of player2
+	p2Cooperate = playermap["p2"]()
 
 	#calculate scores
-	if playerCooperate and computerCooperate:
-		playerScore += 3
-		computerScore += 3
-	if playerCooperate and not computerCooperate:
-		playerScore += 0 #just for show
-		computerScore += 5
-	if not playerCooperate and computerCooperate:
-		playerScore += 5
-		computerScore += 0 #just for show
-		computerIsContent = False
-	if not playerCooperate and not computerCooperate:
-		playerScore += 2
-		computerScore += 2
-		computerIsContent = False
+	if p1Cooperate and p2Cooperate:
+		p1Score += 3
+		p2Score += 3
+	elif p1Cooperate and not p2Cooperate:
+		p1Score += 0 #just for show
+		p2Score += 5
+		if playermap["p1"] == grudger:
+			grudgerIsContent = False
+	elif not p1Cooperate and p2Cooperate:
+		p1Score += 5
+		p2Score += 0 #just for show
+		if playermap["p2"] == grudger:
+			grudgerIsContent = False
+	elif not p1Cooperate and not p2Cooperate:
+		p1Score += 2
+		p2Score += 2
+
 	print("")
-	print("You Cooperated: " + str(playerCooperate))
-	print("Computer Cooperated: " + str(computerCooperate) + "\n")
+	print(playermap["p1"].__name__.title() + " (p1) Cooperated: " + str(p1Cooperate))
+	print(playermap["p2"].__name__.title() + " (p2) Cooperated: " + str(p2Cooperate) + "\n")
 
 	round += 1
-	print("Player Score: " + str(playerScore))
-	print("Computer Score: " + str(computerScore) + "\n")
-	firstRound = False
-	lastPlayerCooperate = playerCooperate
+	print(playermap["p1"].__name__.title() + " (p1) Score: " + str(p1Score))
+	print(playermap["p2"].__name__.title() + " (p2) Score: " + str(p2Score) + "\n")
 
-if playerScore > computerScore:
-	print("You won by " + str(playerScore - computerScore) + " points.")
-elif playerScore == computerScore:
-	print("You tied with the computer.")
+	firstRound = False
+
+	lastp1Cooperate = p1Cooperate
+	lastp2Cooperate = p2Cooperate
+
+if p1Score > p2Score:
+	print(playermap["p1"].__name__.title() + " (p1) won by " + str(p1Score - p2Score) + " points.")
+elif p1Score == p2Score:
+	print(playermap["p1"].__name__.title() + " (p1) tied with " + playermap["p2"].__name__)
 else:
-	print("The computer beat you by " + str(computerScore - playerScore) + " points.")
+	print(playermap["p2"].__name__.title() + " (p2) won by " + str(p2Score - p1Score) + " points.")
